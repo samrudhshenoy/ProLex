cfed3671bc1cccd2adf7a0f42b7b4836b4ffb230
samrudhshenoy <45743500+samrudhshenoy@users.noreply.github.com>
2019-03-23 11:03:52 -0700

    Add files via upload

--- ProLex/src/Main.java
+++ ProLex/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	ArrayList<String> current;
+	ArrayList<String> bad;
+	
+	
+	public Main() {
+		current = new ArrayList<String>();
+		bad = new ArrayList<String>();
 	}
-
+	
 }
+
