1302e12d1ac5c6f2cd79bde4f73c955f8ddb6f52
CalixTang <calix.tang@gmail.com>
2019-03-23 14:05:41 -0700

    Push

--- CalixsCode/ProLex/src/BadWord.java
+++ CalixsCode/ProLex/src/BadWord.java
@@ -7,8 +7,17 @@ public class BadWord {
 	public BadWord(String badWord, String replacement, String message) {
 		this.badWord = badWord;
 		this.replacement = replacement;
+		try {
+			int n = Integer.parseInt(message);
+			if(n<0 || n>FileIO.messages.length - 1) {
+				throw new IllegalArgumentException("message number is invalid for badword.");
+			}else {
+				this.message = FileIO.messages[n];
+			}
+		}catch(NumberFormatException e) {
+			this.message = message;
+		}
 	}
-	
 	public String getBadWord() {
 		return badWord;
 	}
--- CalixsCode/ProLex/src/FileIO.java
+++ CalixsCode/ProLex/src/FileIO.java
@@ -4,13 +4,22 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class FileIO {
+	
+	public static final String lineSep = System.getProperty("line.separator");
+	public static final String fileSep = System.getProperty("file.separator");
+	/**0->offensive (general category, most common)
+	 * 1->racial (race)
+	 * 2->vulgar (sexual)
+	 */
+	public static final String[] messages = {"This word has offensive connotations. Are you sure you want to use this word? Here is a replacement suggestion: ",
+			"This word can be interpreted as a racial slur. Are you sure you want to use this word? Here is a replacement suggestion: ",
+			"This word has vulgar connotations. Are you sure you want to use this word? Here is a replacement suggestion: "};
+	
 	/**Returns the lines of the file in an ArrayList
 	 * 
 	 * @param fileName - the file to read
 	 * @return 
 	 */
-	public static String lineSep = System.getProperty("line.separator");
-	public static String fileSep = System.getProperty("file.separator");
 	public static ArrayList<String> readFile(String fileName) {
 		BufferedReader reader = null;
 		try {
